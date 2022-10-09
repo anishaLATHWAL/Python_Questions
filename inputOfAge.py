6f520518b8eb0d399aae90bf1747bcc54d8b87f9
Anisha <anishalathwalal1983@gmail.com>
2022-10-10 00:56:23 +0530

    To determine the oldest person

---
+++ inputOfAge.py
@@ -0,0 +1,17 @@
+# take input of age from three peoples determine the oldest. 
+
+one = int(input("Enter the first age: "))
+second = int(input("Enter the second age: "))
+third = int(input("Enter the third age: "))
+
+if one > second and one > third :
+    print("one is oldest ")
+
+elif second > third and second > one:
+    print("second is oldest ")
+
+elif third > one and third > second:
+    print("third id oldest ")
+
+else: 
+    print("all the three ages are different.")
