39b30c57189fda748a777326365445dd43f44c99
Anisha <anishalathwalal1983@gmail.com>
2022-10-10 00:48:37 +0530

    To find the greatest number

---
+++ greatestNumber.py
@@ -0,0 +1,15 @@
+# take two inputs from the user(int/float)
+# print the greatest number
+
+first = int(input("enter the first number:"))
+second = int(input("enter the second number:"))
+
+if first > second :
+    print("first is largest")
+
+elif second > first :
+    print("second is largest")
+
+else:
+    print("both are same")
+
