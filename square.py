c64284559759933c8e8c7002202bbf92cd9837f5
Anisha <anishalathwalal1983@gmail.com>
2022-10-12 23:55:26 +0530

    To prove it is square or not.

---
+++ square.py
@@ -0,0 +1,11 @@
+#Take values of length and breadth from user
+#Print if it is square or not
+
+length = int(input("Enter the length here: "))
+breadth = int(input("Enter the breadth here: "))
+
+if length==breadth :
+    print("It is a square")
+
+else:
+    print("It is not a square")
