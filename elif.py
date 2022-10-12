5b7d7dc47a78c36e5d252e1bf08c85c12efe1c92
Anisha <anishalathwalal1983@gmail.com>
2022-10-12 23:59:50 +0530

    How to make use of elif

---
+++ elif.py
@@ -0,0 +1,14 @@
+# elif use
+
+marks = int(input("Enter your marks: "))
+if marks > 90 and marks < 100 :
+     print("Your grade is A")
+
+elif marks >80 and marks <=90 :
+    print("Your grade is B ")
+
+elif marks >70 and marks <= 80 : 
+    print("Your grade is C")
+
+else:
+    print("Failed")
