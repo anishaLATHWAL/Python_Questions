bbcc0e8d0675ba3ac4a669bb0f034745e7a88c78
Anisha <anishalathwalal1983@gmail.com>
2022-10-18 23:31:27 +0530

    Placeholder

---
+++ placeholder.py
@@ -0,0 +1,8 @@
+a = 5
+b = 10
+c = 20
+string = "A:{}, B:{}, C:{}"
+print(string.format(5,6,7)) #filling placeholders
+
+string = "A:{2}, B:{1}, C:{0}"
+print(string.format(5,6,7)) #filling placeholders
