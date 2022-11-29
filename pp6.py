070755597718f0c4ad2600eb86e565a33276af42
Anisha <anishalathwalal1983@gmail.com>
2022-11-29 22:28:23 +0530

    To create tuple

---
+++ pp6.py
@@ -0,0 +1,11 @@
+# name1,name2,name3,name4= str(input("")).split(",")
+# tuple1= (name1,name2,name3,name4)
+# print(tuple1)
+name1= str(input(""))
+name2= str(input(""))
+name3= str(input(""))
+name4= str(input(""))
+
+tuple1=(name1,name2,name3,name4)
+print(tuple1)
+
