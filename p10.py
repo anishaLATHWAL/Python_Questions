46fd8ea7a2ce2c732061a1098994dae8f0cdb201
Anisha <anishalathwalal1983@gmail.com>
2022-11-30 23:33:34 +0530

    For sorting the numbers and for sorting the list in reverse manner..

---
+++ p10.py
@@ -0,0 +1,12 @@
+# For sorting the numbers...
+numbers= [1,2,5,3,10,6,4]
+numbers.sort()
+print(numbers)
+
+
+# sorting inreverse manner..
+numbers.reverse()
+print(numbers)
+
+numbers.sort(reverse=True)
+print(numbers)
